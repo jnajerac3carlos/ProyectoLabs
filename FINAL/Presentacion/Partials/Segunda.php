506f05944947a34dcabc3bb71b07441a050dd003
Cindy Arana <caranaf1@miumg.edu.gt>
2023-08-13 00:43:17 -0600

    [carana]Realizacion de fraccionamiento y principio de responsabilidad unica en presentacion

---
+++ FINAL/Presentacion/Partials/Segunda.php
@@ -0,0 +1,10 @@
+</main>
+<?php require "../Presentacion/Partials/Footer.php" ?>
+
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+
+<?php require "../Presentacion/Partials/Scripts.php" ?>
+</body>
+
+</html>
